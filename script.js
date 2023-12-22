fb31118a3ee1c3c09f2cae6f8347151520b3381e
foveejong <jovefong62@gmail.com>
2023-12-22 15:49:56 +0800

    Obtain user's selection using prompt and make input case-insensitive

---
+++ script.js
@@ -0,0 +1,32 @@
+
+function playRound(playerSelection, computerSelection) {
+    // use prompt() to get player input and store it in playerSelection
+    const playerSelection = prompt("What is your selection?");
+    
+    // make playerSelection case-insensitive
+    playerSelection = playerSelection.toLowerCase();
+    
+    //if win --> str
+    
+    //if lose --> str
+    
+    //if tie --> replay the round --> playRound(playerSelection, computerSelection)
+    
+    // return winner "You Lose! Paper beats Rock"
+}
+
+
+
+
+function getComputerChoice() {
+    //return 'rock', 'paper', 'scissors' as a computer's choice
+} 
+
+
+function game() {
+    //loop playRound for 5 rounds
+
+    //keep score
+    
+    //announce
+}
